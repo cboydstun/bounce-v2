779017239c8f265b323da5d14539b32b15f6c0ec
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-03-08 15:10:10 -0600

    connected to cloud DB

--- db/client.js
+++ db/client.js
@@ -1,12 +1,9 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/bouncev2';
+const connectionString = process.env.DATABASE_URL;
 
 const client = new Client({
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD
+  connectionString
 });
 
 module.exports = client;
